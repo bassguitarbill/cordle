374915293cc6fd69ab7753baf0e92c6200890c50
Bill Rossi <bassguitarbill@gmail.com>
2022-02-10 16:42:37 -0500

    Append the answers to the guesses in one big list

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 
 void pick_random_answer(char* *answer, char* answer_list, int answer_count); 
 void display_board(char* answer, char** guesses, int num_guesses);
-void prompt_for_guess(char** guesses, int guess_index, char* guess_list, int guess_count, char* answer_list, int answer_count);
-int is_valid_guess(char* guess, char* guess_list, int guess_count, char* answer_list, int answer_count);
+void prompt_for_guess(char** guesses, int guess_index, char* guess_list, int guess_count);
+int is_valid_guess(char* guess, char* guess_list, int guess_count);
 
 int main() {
 
@@ -24,6 +24,12 @@ int main() {
   char *an;
   answers(&an, &an_length);
 
+  char* all_valid_guesses = malloc(5 * (ag_length + an_length));
+  memcpy(all_valid_guesses, ag, 5 * ag_length);
+  memcpy(all_valid_guesses + (5 * ag_length), an, 5 * an_length);
+
+  free(ag);
+
   puts("Welcome to Bill's Wordle Clone!");
 
   while (1) {
@@ -36,7 +42,7 @@ int main() {
     int i;
     for (i = 0; i < MAX_GUESSES; i++) {
       display_board(ans, guesses, i);
-      prompt_for_guess(guesses, i, ag, ag_length, an, an_length);
+      prompt_for_guess(guesses, i, all_valid_guesses, ag_length + an_length);
       if (strcmp(guesses[i], ans) == 0) {
         i++;
         victory = 1;
@@ -68,19 +74,18 @@ int main() {
     free(again);
   }
 
-  free(ag);
   free(an);
+  free(all_valid_guesses);
 
   return 0;
 }
 
-// TODO fix this signature so it only takes one list of words
-void prompt_for_guess(char** guesses, int guess_index, char* guess_list, int guess_count, char* answer_list, int answer_count) {
+void prompt_for_guess(char** guesses, int guess_index, char* guess_list, int guess_count) {
   char* guess = malloc(6);
   while (1) {
     puts("What's your guess?");
     scanf("%5s", guess);
-    if (is_valid_guess(guess, guess_list, guess_count, answer_list, answer_count)) {
+    if (is_valid_guess(guess, guess_list, guess_count)) {
       guesses[guess_index] = guess;
       break;
     }
@@ -88,15 +93,11 @@ void prompt_for_guess(char** guesses, int guess_index, char* guess_list, int gue
   }
 }
 
-int is_valid_guess(char* guess, char* guess_list, int guess_count, char* answer_list, int answer_count) {
+int is_valid_guess(char* guess, char* guess_list, int guess_count) {
   // TODO these are sorted and this should be a binary search
   char* guess_from_list = calloc(6, sizeof(char));
   for (int i = 0; i < guess_count; i++) {
     memcpy(guess_from_list, guess_list + (i * 5), 5);
-    if (strcmp(guess, guess_from_list) == 0) return 1;
-  }
-  for (int i = 0; i < answer_count; i++) {
-    memcpy(guess_from_list, answer_list + (i * 5), 5);
     if (strcmp(guess, guess_from_list) == 0) return 1;
   }
   return 0;
